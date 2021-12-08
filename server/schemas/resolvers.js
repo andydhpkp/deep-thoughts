25e3d4235b3f97d478f61dbde6054d4b8ae7c1bf
Andrew Durham <andrewdurham1094@gmail.com>
2021-12-08 10:16:03 -0700

    lesson 1, basic queries

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,46 @@
+const { User, Thought } = require('../models')
+
+/*
+A resolver can accept four arguments in the following order
+1. parent - this is if we used nested resolvers to handle more complicated actions, as it would hold the reference to the resolver that executed the nested resolver function.
+2. args - this is an object of all of the values passed into a query or mutation request as parameters
+3. context - if we were to need the same data to be accessible by all resolvers, such as a logged-in user's status or API access token, this data will come through this context parameter as an object
+4. info - This will contain extra information about an operation's current state
+ */
+
+const resolvers = {
+    Query: {
+        //parent is a placeholder parameter, we need something in that first parameter's spot so we can access the username argument
+        //the ternary operator checks if username exists, if it does, we set params to an object with a username key set to that value. if it doesn't we simply return an empty object
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            //the sort() method returns the data in descending order
+            //we pass the ternary object into the find() method
+            return Thought.find(params).sort({ createdAt: -1 })
+        },
+
+        //get single thought
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id })
+        },
+
+        //get all users
+        users: async () => {
+            return User.find()
+                //the __v omits the property
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts')
+        },
+
+        //get a user by username
+        user: async(parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts')
+        },
+    }
+}
+
+module.exports = resolvers
