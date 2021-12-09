b9441b554ea808b7758a0866732e94d18387cd54
Andrew Durham <andrewdurham1094@gmail.com>
2021-12-09 13:21:29 -0700

    added basic mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Thought } = require('../models')
+const { AuthenticationError } = require('apollo-server-express')
 
 /*
 A resolver can accept four arguments in the following order
@@ -40,6 +41,29 @@ const resolvers = {
                 .populate('friends')
                 .populate('thoughts')
         },
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+          
+            return user;
+        },
+        login: async(parent, { email, password}) => {
+            const user = await User.findOne({ email })
+
+            if(!user) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            const correctPw = await user.isCorrectPassword(password)
+
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            return user;
+
+        }
     }
 }
 
