52864598a35182c3ed2736d9c2776041c9d64127
Andrew Durham <andrewdurham1094@gmail.com>
2021-12-09 14:12:04 -0700

    finished lesson 2, added CRUD operations in mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -80,6 +80,50 @@ const resolvers = {
 
             return { token, user };
 
+        },
+        addThought: async(parent, args, context) => {
+            if(context.user) {
+                const thought = await Thought.create({ ...args, username: context.user.username })
+
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { thoughts: thought._id }},
+                    //Remember, without the { new: true } flag, Mongo would return the original document instead of the updated document
+                    { new: true }
+                );
+
+                return thought;
+            }
+
+            throw new AuthenticationError('You need to be logged in!')
+        },
+        addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+            if (context.user) {
+                const updatedThought = await Thought.findOneAndUpdate(
+                    //be sure to copy the _id property from one of the test thoughts you created
+                    { _id: thoughtId },
+                    { $push: { reactions: { reactionBody, username: context.user.username}}},
+                    { new: true, runValidators: true }
+                )
+
+                return updatedThought
+            }
+
+            throw new AuthenticationError('You need to be logged in!')
+        },
+        addFriend: async (parent, { friendId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    //A user can't be friends with the same person twice, so use addToSet instead of push
+                    { $addToSet: { friends: friendId }},
+                    { new: true }
+                ).populate('friends')
+
+                return updatedUser
+            }
+
+            throw new AuthenticationError('You need to be logged in!')
         }
     }
 }
