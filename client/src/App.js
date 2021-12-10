e24a9499fc5100d8f83478d8247bc3fd5b59ff03
Andrew Durham <andrewdurham1094@gmail.com>
2021-12-09 17:19:30 -0700

    finished lesson 4, added mulitple components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -24,13 +31,23 @@ function App() {
     //enable our application to interact with out Apollo client instance
     //Wrap everything in ApolloProvider because we're passing the client variable in as the value for the client prop in the provider, everything between the tags will eventually have access to the server's API through the client
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
         </div>
         <Footer />
       </div>
+      </Router>
     </ApolloProvider>
   );
 }
